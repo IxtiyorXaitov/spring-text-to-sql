8916a2bb0c63fcf4408e96b2d0051c9ffba01e5f
ixtiyorxaitov <ikhtiyordev@gmail.com>
2025-07-17 22:50:11 +0500

    first commit

---
+++ src/test/java/com/ixtiyor/springtexttosql/SpringTextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixtiyor.springtexttosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTextToSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
